40bdff65f3a5a24d8c148e729584a0942285cce8
CCNUAI <101306075+CCNUAI@users.noreply.github.com>
2023-06-29 15:15:10 +0800

    Add files via upload

---
+++ js/app.0df02320.js
@@ -0,0 +1,248 @@
+(function () {
+    "use strict";
+    var t = {
+        8885: function (t, e, n) {
+            var o = n(6369), i = n(8499), c = n.n(i), a = function () {
+                var t = this, e = t._self._c;
+                return e("div", {attrs: {id: "app"}}, [e("HelloWorld")], 1)
+            }, l = [], s = function () {
+                var t = this, e = t._self._c;
+                return e("el-container", {staticStyle: {height: "100vh"}}, [e("el-header", {
+                    staticClass: "header",
+                    attrs: {height: "10vh"}
+                }, [e("el-image", {
+                    staticStyle: {
+                        width: "auto",
+                        height: "8vh",
+                        "margin-left": "2vw",
+                        "margin-right": "2vw"
+                    }, attrs: {src: t.logo_img}
+                }), e("el-menu", {
+                    attrs: {
+                        "default-active": t.activeIndex,
+                        mode: "horizontal",
+                        "active-text-color": "#16356C"
+                    }, on: {select: t.handleSelect}
+                }, [e("el-menu-item", {
+                    staticClass: "el-menu-demo",
+                    attrs: {index: "0"}
+                }, [t._v("教育数字化")]), e("el-menu-item", {
+                    staticClass: "el-menu-demo",
+                    attrs: {index: "1"}
+                }, [t._v("高等教育")]), e("el-menu-item", {
+                    staticClass: "el-menu-demo",
+                    attrs: {index: "2"}
+                }, [t._v("基础教育")])], 1)], 1), e("el-main", [e("div", {staticClass: "content-title"}, [t._v(t._s(t.now.title))]), e("el-carousel", {
+                    attrs: {
+                        interval: 3e3,
+                        type: "card",
+                        height: "55vh"
+                    }, on: {change: t.getCardChange}
+                }, t._l(t.now.items, (function (n, o) {
+                    return e("el-carousel-item", {key: n.title}, [e("div", {
+                        staticClass: "card",
+                        style: {opacity: o == t.nowCardIndex ? .9 : .4},
+                        on: {
+                            click: function (e) {
+                                return t.tapImage(o)
+                            }
+                        }
+                    }, [e("el-image", {
+                        staticClass: "card-img",
+                        staticStyle: {height: "55vh"},
+                        attrs: {src: n.img},
+                        on: {
+                            click: function (e) {
+                                return t.tapImage(o)
+                            }
+                        }
+                    }, [t._v("> ")]), e("div", {staticClass: "card-subTitle"}, [t._v(t._s(n.title))])], 1)])
+                })), 1)], 1), e("el-footer", [t._v("华中师范大学人工智能教育学部")]), e("el-dialog", {
+                    attrs: {
+                        fullscreen: !0,
+                        visible: t.dialogVisible
+                    }, on: {
+                        "update:visible": function (e) {
+                            t.dialogVisible = e
+                        }, close: t.closeDialog
+                    }
+                }, [e("span", {
+                    staticClass: "dialog-title",
+                    attrs: {slot: "title"},
+                    slot: "title"
+                }, [t._v(t._s(t.chosenItem.title))]), e("video", {
+                    attrs: {
+                        id: "video",
+                        width: "70%",
+                        controls: "",
+                        autoplay: !0,
+                        src: t.chosenItem.vedio
+                    }
+                })])], 1)
+            }, r = [];
+            const d = [{
+                title: "教育数字化",
+                items: [{
+                    title: "教育数字化转型战略行动",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/zy.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/4fa16890243791581946243322/qcMbdFdv3RQA.mp4"
+                }, {
+                    title: "学校教育数字化转型规划",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/zx.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/5b5f81da243791581946757412/HE1iHV6MZuUA.mp4"
+                }, {
+                    title: "世界数字教育大会",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/dahui.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/bc70603e3270835009811440558/hSdhB87SPg8A.mp4"
+                }]
+            }, {
+                title: "高等教育",
+                items: [{
+                    title: "数字化卓越教师培养体系的构建与实践",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/teacher.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/4f8ab1fe3270835009813162744/iQO1qOq1HKwA.mp4"
+                }, {
+                    title: "人工智能+教育”研究生高质量培养体系的探索与实践",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/yjs.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/7b56d0c53270835009810971818/l7atjyoorawA.mp4"
+                }, {
+                    title: "数字人文实践中的“云上中文”平台构筑与应用",
+                    img: "https://1318089688.vod2.myqcloud.com/af32055evodtranscq1318089688/02c9f3613270835009812198217/coverBySnapshot_10_0.jpg",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/02c9f3613270835009812198217/iNcYDndytH4A.mp4"
+                }]
+            }, {
+                title: "基础教育",
+                items: [{
+                    title: '"互联网+”支撑省域基础教育优质均衡发展',
+                    img: "https://1318089688.vod2.myqcloud.com/af32055evodtranscq1318089688/047baf753270835009812207160/coverBySnapshot_10_0.jpg",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/047baf753270835009812207160/k6DnqLk0hwIA.mp4"
+                }, {
+                    title: "AI+三个课堂“开好课创新实践片",
+                    img: "https://oss.funcfang.cn/ccnu/yan0/ai3.png",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/c13124e43270835009811658382/1f7ZYSmunxYA.mp4"
+                }, {
+                    title: "人工智能引领的科学教育创新",
+                    img: "https://1318089688.vod2.myqcloud.com/af32055evodtranscq1318089688/106a0e9c3270835009812740372/coverBySnapshot_10_0.jpg",
+                    vedio: "https://1318089688.vod2.myqcloud.com/d84817b6vodcq1318089688/106a0e9c3270835009812740372/aj3O0GhclIMA.mp4"
+                }]
+            }];
+            var u = {
+                    name: "HelloWorld", data() {
+                        return {
+                            activeIndex: "0",
+                            logo_img: "https://oss.funcfang.cn/ccnu/ccnu_logo.png",
+                            dialogVisible: !1,
+                            now: d[0],
+                            chosenItem: d[0].items[0],
+                            nowCardIndex: 0
+                        }
+                    }, methods: {
+                        handleSelect(t) {
+                            this.now = d[t], this.activeIndex = t + ""
+                        }, getCardChange(t) {
+                            this.nowCardIndex = t
+                        }, tapImage(t) {
+                            t == this.nowCardIndex ? (this.dialogVisible = !0, this.chosenItem = this.now.items[t]) : setTimeout((() => {
+                                this.dialogVisible = !0, this.chosenItem = this.now.items[t]
+                            }), 700)
+                        }, closeDialog() {
+                            const t = document.getElementById("video");
+                            t.pause()
+                        }
+                    }
+                }, m = u, v = n(1001), f = (0, v.Z)(m, s, r, !1, null, "1870551c", null), h = f.exports,
+                p = {name: "App", components: {HelloWorld: h}}, g = p, y = (0, v.Z)(g, a, l, !1, null, null, null),
+                b = y.exports;
+            o["default"].use(c()), new o["default"]({el: "#app", render: t => t(b)})
+        }
+    }, e = {};
+
+    function n(o) {
+        var i = e[o];
+        if (void 0 !== i) return i.exports;
+        var c = e[o] = {id: o, loaded: !1, exports: {}};
+        return t[o].call(c.exports, c, c.exports, n), c.loaded = !0, c.exports
+    }
+
+    n.m = t, function () {
+        n.amdO = {}
+    }(), function () {
+        var t = [];
+        n.O = function (e, o, i, c) {
+            if (!o) {
+                var a = 1 / 0;
+                for (d = 0; d < t.length; d++) {
+                    o = t[d][0], i = t[d][1], c = t[d][2];
+                    for (var l = !0, s = 0; s < o.length; s++) (!1 & c || a >= c) && Object.keys(n.O).every((function (t) {
+                        return n.O[t](o[s])
+                    })) ? o.splice(s--, 1) : (l = !1, c < a && (a = c));
+                    if (l) {
+                        t.splice(d--, 1);
+                        var r = i();
+                        void 0 !== r && (e = r)
+                    }
+                }
+                return e
+            }
+            c = c || 0;
+            for (var d = t.length; d > 0 && t[d - 1][2] > c; d--) t[d] = t[d - 1];
+            t[d] = [o, i, c]
+        }
+    }(), function () {
+        n.n = function (t) {
+            var e = t && t.__esModule ? function () {
+                return t["default"]
+            } : function () {
+                return t
+            };
+            return n.d(e, {a: e}), e
+        }
+    }(), function () {
+        n.d = function (t, e) {
+            for (var o in e) n.o(e, o) && !n.o(t, o) && Object.defineProperty(t, o, {enumerable: !0, get: e[o]})
+        }
+    }(), function () {
+        n.g = function () {
+            if ("object" === typeof globalThis) return globalThis;
+            try {
+                return this || new Function("return this")()
+            } catch (t) {
+                if ("object" === typeof window) return window
+            }
+        }()
+    }(), function () {
+        n.o = function (t, e) {
+            return Object.prototype.hasOwnProperty.call(t, e)
+        }
+    }(), function () {
+        n.r = function (t) {
+            "undefined" !== typeof Symbol && Symbol.toStringTag && Object.defineProperty(t, Symbol.toStringTag, {value: "Module"}), Object.defineProperty(t, "__esModule", {value: !0})
+        }
+    }(), function () {
+        n.nmd = function (t) {
+            return t.paths = [], t.children || (t.children = []), t
+        }
+    }(), function () {
+        var t = {143: 0};
+        n.O.j = function (e) {
+            return 0 === t[e]
+        };
+        var e = function (e, o) {
+            var i, c, a = o[0], l = o[1], s = o[2], r = 0;
+            if (a.some((function (e) {
+                return 0 !== t[e]
+            }))) {
+                for (i in l) n.o(l, i) && (n.m[i] = l[i]);
+                if (s) var d = s(n)
+            }
+            for (e && e(o); r < a.length; r++) c = a[r], n.o(t, c) && t[c] && t[c][0](), t[c] = 0;
+            return n.O(d)
+        }, o = self["webpackChunkccnu_for_ai"] = self["webpackChunkccnu_for_ai"] || [];
+        o.forEach(e.bind(null, 0)), o.push = e.bind(null, o.push.bind(o))
+    }();
+    var o = n.O(void 0, [998], (function () {
+        return n(8885)
+    }));
+    o = n.O(o)
+})();
+//# sourceMappingURL=app.0df02320.js.map
